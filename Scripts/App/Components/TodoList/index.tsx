4172674e1640daa453a9e5a893d3bea2cb675b31
mariocatch <mariocatch@gmail.com>
2017-05-12 22:13:44 -0400

    Add {key} to todolist (fix react warning)

--- Scripts/App/Components/TodoList/index.tsx
+++ Scripts/App/Components/TodoList/index.tsx
@@ -7,11 +7,15 @@ interface ITodoListProps {
 }
 
 export class TodoList extends React.Component<ITodoListProps, any> {
+    constructor(props: ITodoListProps) {
+        super(props);
+    }
+
     public render() {
         return (
             <ul>
-                {this.props.items.map(i => {
-                    return <TodoItem name={i.name} />
+                {this.props.items.map((todo, i) => {
+                    return <TodoItem key={i} name={todo.name} />
                 })}
             </ul>
         );
